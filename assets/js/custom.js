94ac0c904389e7468b5cddb299df4c964fbe24e2
治中 鄭 <joecheng@best-epitaxy.com>
2023-11-08 16:59:25 +0800

    RWD FOOTER HISTOR 調整，slideshow 文字背景增加

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -117,7 +117,7 @@ jQuery(document).ready(function ($) {
   }
 
   $(".Modern-Slider").slick({
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 10000,
     speed: 600,
     slidesToShow: 1,
